dedade042d7072dcfca44c3c9331d209d04b2bb2
eager7 <panchangtao@gmail.com>
2016-06-28 16:47:32 +0800

    Html Parser

--- spider/ScreenCapture/screen.py
+++ spider/ScreenCapture/screen.py
@@ -4,41 +4,27 @@
 import signal,sys
 import re, bs4
 import requests
+from mHtmlParser import mHtmlParser
+
 from mDbg import *
 
-url = "http://bbs.feng.com/thread-htm-fid-68.html"
-url_1024 = "http://1024.hegongchang.red/pw/thread.php?fid=17"
-url_test = "http://www.54new.com/index.html"
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+url = "https://1024coder.com"
+url2 = "http://bbs.feng.com/thread-htm-fid-482.html/"
 
 def sigint_handler(signum,frame):
     print "main-thread exit"
     sys.exit()
 
-def get_capture(url):
-    html = requests.get(url)
-    link_list = re.findall(r"(<a href.*onclick.*</a>)" ,html.text)
-    #link_list = re.findall(r"")
-    for url_l in link_list:
-        rel, err = re.subn(r".*</em>", "", url_l)
-        rel, err = re.subn(r".*<a href=", "", rel)
-        rel, err = re.subn(r".*title.*", "", rel)
-        rel, err = re.subn(r".*style.*", "", rel)
-        #rel, err = re.subn(r"onclick=.*", "", rel)
-        NOT_Printf(True, rel)
-
-def get1024_txt(url):
-    resp = requests.get(url)
-    soup = bs4.BeautifulSoup(resp.text)
-    links = [a.attrs.get('href') for a in soup.select('dev.ui stackable grid a[href^*]')]
-    for url_l in links:
-        print url_l
-
 
 if __name__ == '__main__':
     DBG_Printf(True, "My First Spider Program")
-    reload(sys)
-    sys.setdefaultencoding("utf-8")
     signal.signal(signal.SIGINT, sigint_handler)
 
-    #get_capture(url)
-    get1024_txt(url_test)
+    html = requests.get(url2)
+    mParser = mHtmlParser(url2)
+    mParser.feed(html.text)
+    mParser.close()
+    mParser.display()
