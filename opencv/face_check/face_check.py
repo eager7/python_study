1d92fe36b436eb22292bd4f6119e919e2151aa0d
jiangtao.zhao <jiangtao.zhao@kuang-chi.com>
2016-07-07 07:35:38 +0000

    opencv camera

---
+++ opencv/face_check/face_check.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cv2.cv as cv
+
+if __name__ == '__main__':
+    capture=cv.CaptureFromCAM(0)
+    dst = cv.CreateImage((int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH)),
+                        int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT))), 8, 1)
+    while True:
+        frame=cv.QueryFrame(capture)
+        cv.CvtColor(frame, dst, cv.CV_BGR2GRAY)
+        cv.Canny(dst, dst, 125, 350)
+        cv.Threshold(dst, dst, 128, 255, cv.CV_THRESH_BINARY_INV)
+        cv.ShowImage("The Video", frame)
+        cv.ShowImage("The Dst", dst)
+        c = cv.WaitKey(1)
+        if c == 27: #Esc on Windows
+            break
--- opencv/read_video/read_video.py
+++ opencv/read_video/read_video.py
@@ -5,19 +5,15 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-    capture = cv2.VideoCapture(0)
-
-    #fourcc = cv2.VideoWriter_fourcc(*'XVID') #this version is not support this method
-    fourcc = cv2.cv.CV_FOURCC(*'XVID')
-    out = cv2.VideoWriter('output.avi', fourcc, 30.0, (640,480))
+    capture = cv2.VideoCapture(r'/home/firefly/pct/python_study/opencv/camera/output.avi')
+    print capture.isOpened()
     while capture.isOpened():
         ret, frame = capture.read()
         if ret == True:
             cv2.imshow("Camera", frame)
-            #frame_w = cv2.flip(frame, 0) #overturn picture
-            out.write(frame)
+        else:
+            break
         if cv2.waitKey(1)&0xff == ord('q'):
             break
     capture.release()
-    out.release()
     cv2.destroyAllWindows()
