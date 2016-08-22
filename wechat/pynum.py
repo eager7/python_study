0b248e5845990cbd729034501193aebb4cdd6dfb
eager7 <panchagntao@gmail.com>
2016-08-22 03:11:40 -0700

    pynum例程

--- wechat/pynum.py
+++ wechat/pynum.py
@@ -1,5 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-if __name__ == '__main__':
+import numpy as np
+import json
+from collections import Counter
+import pandas as pd
+from pandas import DataFrame,Series
+import matplotlib.pyplot as plt
+
+def main():
     print 'python num test'
+    with open('contact.txt', 'r') as f:
+        records = json.load(f)
+    print len(records)
+    citys = [record['City'] for record in records if 'City' in record]
+    print citys
+    counts = Counter(citys)
+    for x in counts.most_common():
+        print x[0],x[1]
+    print '----------------'*10
+
+    frame = DataFrame(records)
+    print frame['City'].value_counts()
+    series = frame['City'].value_counts()
+    series.plot(kind='barh', rot=0)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+    raw_input('entry to exit')
