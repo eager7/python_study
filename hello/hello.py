ad1125175f68d7b7180e9b83d3555c0e0eb1734f
eager7 <panchangtao@gmail.com>
2022-10-12 07:40:02 +0800

    python3

--- hello/hello.py
+++ hello/hello.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python
 
-import binascii
-
 if __name__ == '__main__':
-    print 'hello world'
+    print('hello world')
     a = 120
-    print bytes(a)
-    print str(a)
+    print(bytes(a))
+    print(str(a))
 
-    print bin(a)
-    print '%c'%a
-    print chr(a)
+    print(bin(a))
+    print('%c' % a)
+    print(chr(a))
 
     b = 'x'
 
-    print ord(b)
-
+    print(ord(b))
 
     lists = [1,2,5,8,7,3,4,9]
-    print lists.sort()
-    print lists
+    print(lists.sort())
+    print(lists)
     lists = lists[1:-1]
-    print lists
-    print sum(lists), len(lists)
-    print sum(lists)/len(lists)
+    print(lists)
+    print(sum(lists), len(lists))
+    print(sum(lists) / len(lists))
--- hello/waitkey.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import cv2.cv as cv
-
-if __name__ == '__main__':
-    while True:
-	print("%d")%cv.WaitKey(1000)
-
